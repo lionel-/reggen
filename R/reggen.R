5377c41f5b0d0cb2735c2e10fe5db6da43646ee0
Lionel Henry <lionel.hry@gmail.com>
2018-08-07 09:59:41 +0200

    Merge files

--- R/reggen.R
+++ R/reggen.R
@@ -15,3 +15,25 @@ use_registered <- function(gen, ...) {
   # Could .Call() into Rf_eval() to avoid one extra call frame
   eval_bare(quote(gen(...)), env)
 }
+
+
+mygeneric_impl <- function(...) {
+  UseMethod("mygeneric")
+}
+
+#' A pure registry-dispatch generic
+#'
+#' @param ... Generic arguments.
+#' @export
+#' @examples
+#' mygeneric.character <- function(...) stop("Never Called")
+#' mygeneric("foo")
+mygeneric <- function(...) {
+  return(use_registered(mygeneric_impl, ...))
+  UseMethod("mygeneric") # So roxygen recognises the generic
+}
+
+#' @export
+mygeneric.character <- function(x, ...) {
+  cat("Registered character method\n")
+}
